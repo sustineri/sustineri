2a4b91697c7a174487b68788bcd65c40661e83bc
Luca Christen <lucachristen94@gmail.com>
2018-09-15 12:33:15 +0200

    Parse amount in product name and create proper JSON
    Parse the amount in a product title if it contains a unit which
    isn't already represented in the amount of the receipt (e.g.
    Chicken 250g, Amount: 1).
    
    Put this in a dictionary to create a proper JSON.

--- app/pdf/coop_pdf_parser.py
+++ app/pdf/coop_pdf_parser.py
@@ -6,8 +6,8 @@ import re
 class CoopPdfParser:
     START_ROW = 5
     STOP_ROW = 'Total CHF'
-    AMOUNT_INDEX = 1
-    PRICE_INDEX = 2
+    AMOUNT_INDEX = 0
+    PRICE_INDEX = 1
 
     @staticmethod
     def parse(pdf_stream):
@@ -25,8 +25,59 @@ class CoopPdfParser:
             product_name = line[:line.find('  ')].strip()
             product_data = re.sub('\s+', ' ', line[len(product_name):]).strip().split(' ')
 
-            products.append((product_name,
-                             product_data[CoopPdfParser.AMOUNT_INDEX],
-                             product_data[CoopPdfParser.PRICE_INDEX]))
+            amount = float(product_data[CoopPdfParser.AMOUNT_INDEX])
+            if amount == 1.0:
+                amount = CoopPdfParser.__parse_amount(product_name)
+
+            record = {}
+            record['productName'] = product_name
+            record['price'] = float(product_data[CoopPdfParser.PRICE_INDEX])
+            record['amount'] = amount
+
+            products.append(record)
 
         return products
+
+    @staticmethod
+    def __parse_amount(product_name):
+        product_name = product_name.lower()
+
+        match = re.search(r'[0-9]x[0-9]+kg', product_name)
+        if match is not None:
+            amount = match.group(0).split('x')
+            return int(amount[0]) * float(amount[1][:-len('kg')])
+
+        match = re.search(r'[0-9]+kg', product_name)
+        if match is not None:
+            return float(match.group(0)[:-len('kg')])
+
+        match = re.search(r'[0-9]x[0-9]+g', product_name)
+        if match is not None:
+            amount = match.group(0).split('x')
+            return int(amount[0]) * float(amount[1][:-len('g')]) / 1000
+
+        match = re.search(r'[0-9]+g', product_name)
+        if match is not None:
+            print(match.group(0))
+            return float(match.group(0)[:-len('g')]) / 1000
+
+        match = re.search(r'[0-9]x[0-9]+l', product_name)
+        if match is not None:
+            amount = match.group(0).split('x')
+            return int(amount[0]) * float(amount[1][:-len('l')])
+
+        match = re.search(r'[0-9]+l', product_name)
+        if match is not None:
+            return float(match.group(0)[:-len('l')])
+
+        match = re.search(r'[0-9]x[0-9]+cl', product_name)
+        if match is not None:
+            amount = match.group(0).split('x')
+            return int(amount[0]) * float(amount[1][:-len('cl')]) / 100
+
+        match = re.search(r'[0-9]+cl', product_name)
+        if match is not None:
+            return float(match.group(0)[:-len('cl')]) / 100
+
+        return 1.0
+
