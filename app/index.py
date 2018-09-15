dbd0db5793b4a4ae8d6d9ea37d9d3ec17d1f5a59
Arik Guggenheim <arik.guggenheim@mimacom.com>
2018-09-15 14:01:34 +0200

    added upload page and improved chart

--- app/index.py
+++ app/index.py
@@ -37,6 +37,10 @@ def index():
 def overview():
     return render_template('overview.html')
 
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
