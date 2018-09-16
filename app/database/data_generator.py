f29ec7ce4abee8a4c4f148e2d8535a425f380a88
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2018-09-16 08:42:14 +0200

    Add new dummy data

--- app/database/data_generator.py
+++ app/database/data_generator.py
@@ -28,31 +28,26 @@ def get_food_name():
     return food[randint(0, len(food)-1)]
 
 
-def get_receipt_id():
-    return randint(1, RECEIPT_SIZE)
-
-
 if __name__ == '__main__':
     print('''
-INSERT INTO users (username, password) VALUES ('admin', 'admin');
-INSERT INTO users (username, password) VALUES ('emanuele', 'pw');
-INSERT INTO users (username, password) VALUES ('arik', 'pw');
-INSERT INTO users (username, password) VALUES ('luca', 'pw');
-INSERT INTO users (username, password) VALUES ('bedri', 'pw');
-INSERT INTO users (username, password) VALUES ('peter', 'pw');
-INSERT INTO users (username, password) VALUES ('david', 'pw');
-INSERT INTO users (username, password) VALUES ('dani', 'pw');
-INSERT INTO users (username, password) VALUES ('savino', 'pw');
-INSERT INTO users (username, password) VALUES ('markus', 'pw');
+INSERT INTO users (user_id, username, password) VALUES (1, 'admin', 'admin');
+INSERT INTO users (user_id, username, password) VALUES (2, 'emanuele', 'pw');
+INSERT INTO users (user_id, username, password) VALUES (3, 'arik', 'pw');
+INSERT INTO users (user_id, username, password) VALUES (4, 'luca', 'pw');
+INSERT INTO users (user_id, username, password) VALUES (5, 'bedri', 'pw');
+INSERT INTO users (user_id, username, password) VALUES (6, 'peter', 'pw');
+INSERT INTO users (user_id, username, password) VALUES (7, 'david', 'pw');
+INSERT INTO users (user_id, username, password) VALUES (8, 'dani', 'pw');
+INSERT INTO users (user_id, username, password) VALUES (9, 'savino', 'pw');
+INSERT INTO users (user_id, username, password) VALUES (10, 'markus', 'pw');
     ''')
-
     print()
 
     receipt_id = 1
     for week_nr in range(NR_OF_WEEKS):
         # receipts
         for i in range(generate_nr_of_receipts(8, 10)):
-            print(f'''INSERT INTO receipts (upload_week, user_id) VALUES ({week_nr}, {get_user_id()});''')
+            print(f'''INSERT INTO receipts (receipt_id, upload_week, user_id) VALUES ({receipt_id}, {week_nr}, {get_user_id()});''')
 
             # items
             for k in range(3):
@@ -60,7 +55,7 @@ INSERT INTO users (username, password) VALUES ('markus', 'pw');
 
             receipt_id += 1
 
-        print(f'''INSERT INTO receipts (upload_week, user_id) VALUES ({week_nr}, 1);''')
+        print(f'''INSERT INTO receipts (receipt_id, upload_week, user_id) VALUES ({receipt_id}, {week_nr}, 1);''')
 
         # items
         for k in range(3):
